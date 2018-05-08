8a9f6ac97b11ee2390e1a200baf4ab289f7548f6
Adam Scarr <adam@vektah.net>
2018-05-08 22:18:38 +1000

    big sweaty test refactor

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,142 +1,38 @@
 package lexer
 
 import (
-	"io/ioutil"
-	"strconv"
-	"strings"
 	"testing"
 
-	"github.com/vektah/graphql-parser"
-	"gopkg.in/yaml.v2"
+	"github.com/vektah/graphql-parser/spec"
 )
 
 func TestLexer(t *testing.T) {
-	b, err := ioutil.ReadFile("spec.yml")
-	if err != nil {
-		panic(err)
-	}
-	var tests testcase
-	err = yaml.Unmarshal(b, &tests)
-	if err != nil {
-		t.Errorf("unable to load spec.yml: %s", err.Error())
-		return
-	}
+	spec.Test(t, "spec.yml", func(t *testing.T, input string) spec.Spec {
+		l := New(input)
 
-	for name, test := range tests {
-		t.Run(name, test.Run)
-	}
-}
+		ret := spec.Spec{}
+		for {
+			tok, err := l.ReadToken()
 
-type testcase map[string]test
-
-type test struct {
-	Input    string
-	Subtests map[string]test
-	Error    struct {
-		Message  string
-		Location struct {
-			Line   int
-			Column int
-		}
-	}
-	Tokens []struct {
-		Kind   string
-		Value  string
-		Start  int
-		End    int
-		Line   int
-		Column int
-	}
-}
-
-func (test *test) Run(t *testing.T) {
-	for name, child := range test.Subtests {
-		t.Run(name, child.Run)
-	}
-
-	if test.Input == "" {
-		return
-	}
-
-	l := New(test.Input)
-	var tokens []Token
-	var error *graphql_parser.Error
-
-	for {
-		tok, err := l.ReadToken()
-
-		if err != nil {
-			error = err
-			break
-		}
-
-		if tok.Kind == EOF {
-			break
-		}
-
-		tokens = append(tokens, tok)
-	}
-
-	t.Logf("input: %s", strconv.Quote(test.Input))
-	if error != nil {
-		t.Logf("error: %s", error.Error())
-	}
-	t.Log("tokens: ")
-	for _, tok := range tokens {
-		t.Logf("  - %s", tok.String())
-	}
-	t.Logf("  - <EOF>")
-
-	if test.Error.Message == "" {
-		if error != nil {
-			t.Errorf("unexpected error %s", error.Error())
-		}
-	} else if error == nil {
-		t.Errorf("expected error %s but got none", test.Error.Message)
-	} else {
-		if error.Message != test.Error.Message {
-			t.Errorf("wrong error returned\nexpected: %s\ngot:      %s", test.Error.Message, error.Message)
-		}
-
-		if error.Locations[0].Column != test.Error.Location.Column || error.Locations[0].Line != test.Error.Location.Line {
-			t.Errorf(
-				"wrong error location:\nexpected: line %d column %d\ngot:      line %d column %d",
-				test.Error.Location.Line,
-				test.Error.Location.Column,
-				error.Locations[0].Line,
-				error.Locations[0].Column,
-			)
-		}
-	}
-
-	if len(test.Tokens) != len(tokens) {
-		var tokensStr []string
-		for _, t := range tokens {
-			tokensStr = append(tokensStr, t.String())
-		}
-		t.Errorf("token count mismatch, got: \n%s", strings.Join(tokensStr, "\n"))
-	} else {
-		for i, tok := range tokens {
-			expected := test.Tokens[i]
-
-			if !strings.EqualFold(strings.Replace(expected.Kind, "_", "", -1), tok.Kind.Name()) {
-				t.Errorf("token[%d].kind should be %s, was %s", i, expected.Kind, tok.Kind.Name())
+			if err != nil {
+				ret.Error = err
+				break
 			}
-			if expected.Value != "undefined" && expected.Value != tok.Value {
-				t.Errorf("token[%d].value incorrect\nexpected: %s\ngot:      %s", i, strconv.Quote(expected.Value), strconv.Quote(tok.Value))
+
+			if tok.Kind == EOF {
+				break
 			}
-			if expected.Start != 0 && expected.Start != tok.Start {
-				t.Errorf("token[%d].start should be %d, was %d", i, expected.Start, tok.Start)
-			}
-			if expected.End != 0 && expected.End != tok.End {
-				t.Errorf("token[%d].end should be %d, was %d", i, expected.End, tok.End)
-			}
-			if expected.Line != 0 && expected.Line != tok.Line {
-				t.Errorf("token[%d].line should be %d, was %d", i, expected.Line, tok.Line)
-			}
-			if expected.Column != 0 && expected.Column != tok.Column {
-				t.Errorf("token[%d].column should be %d, was %d", i, expected.Column, tok.Column)
-			}
+
+			ret.Tokens = append(ret.Tokens, spec.Token{
+				Kind:   tok.Kind.Name(),
+				Value:  tok.Value,
+				Line:   tok.Line,
+				Column: tok.Column,
+				Start:  tok.Start,
+				End:    tok.End,
+			})
 		}
-	}
+
+		return ret
+	})
 }
--- parser/query_test.go
+++ parser/query_test.go
@@ -1,94 +1,17 @@
 package parser
 
 import (
-	"io/ioutil"
-	"strconv"
-	"strings"
 	"testing"
 
-	"github.com/andreyvit/diff"
-	yaml "gopkg.in/yaml.v2"
+	"github.com/vektah/graphql-parser/spec"
 )
 
 func TestQueryDocument(t *testing.T) {
-	b, err := ioutil.ReadFile("queryspec.yml")
-	if err != nil {
-		panic(err)
-	}
-	var tests testcase
-	err = yaml.Unmarshal(b, &tests)
-	if err != nil {
-		t.Errorf("unable to load spec.yml: %s", err.Error())
-		return
-	}
-
-	for name, testcase := range tests {
-		t.Run(name, func(t *testing.T) {
-			for name, test := range testcase {
-				t.Run(name, test.Run)
-			}
-		})
-	}
-}
-
-type testcase map[string]map[string]test
-
-type test struct {
-	Input    string
-	Subtests map[string]test
-	Error    struct {
-		Message  string
-		Location struct {
-			Line   int
-			Column int
+	spec.Test(t, "queryspec.yml", func(t *testing.T, input string) spec.Spec {
+		doc, err := ParseQuery(input)
+		return spec.Spec{
+			Error: err,
+			AST:   Dump(doc),
 		}
-	}
-	AST string
-}
-
-func (test *test) Run(t *testing.T) {
-	for name, child := range test.Subtests {
-		t.Run(name, child.Run)
-	}
-
-	doc, err := ParseQuery(test.Input)
-	ast := strings.TrimSpace(Dump(doc))
-	test.AST = strings.TrimSpace(test.AST)
-
-	if test.Error.Message == "" {
-		if err != nil {
-			t.Errorf("unexpected error %s", err.Error())
-		}
-	} else if err == nil {
-		t.Errorf("missing error\nexpected: %s\ngot:      <nil>", test.Error.Message)
-	} else {
-		if err.Message != test.Error.Message {
-			t.Errorf("wrong error returned\nexpected: %s\ngot:      %s", test.Error.Message, err.Message)
-		}
-
-		if err.Locations[0].Column != test.Error.Location.Column || err.Locations[0].Line != test.Error.Location.Line {
-			t.Errorf(
-				"wrong error location:\nexpected: line %d column %d\ngot:      line %d column %d",
-				test.Error.Location.Line,
-				test.Error.Location.Column,
-				err.Locations[0].Line,
-				err.Locations[0].Column,
-			)
-		}
-	}
-
-	if test.AST != "" && ast != test.AST {
-		diff := diff.LineDiff(test.AST, ast)
-		if diff != "" {
-			t.Errorf("AST mismatch:\n%s", diff)
-		}
-	}
-
-	if t.Failed() {
-		t.Logf("input: %s", strconv.Quote(test.Input))
-		if err != nil {
-			t.Logf("error: %s", err.Error())
-		}
-	}
-
+	})
 }
