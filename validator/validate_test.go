e0f40acfc3adc44130d9d7c41fe155edc95e1695
Adam Scarr <adam.scarr@99designs.com>
2018-07-10 14:24:08 +1000

    Add deviations file for small differences between spec

--- validator/validate_test.go
+++ validator/validate_test.go
@@ -5,27 +5,12 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/vektah/gqlparser"
 	"github.com/vektah/gqlparser/errors"
 	yaml "gopkg.in/yaml.v2"
 )
-
-func TestValidate(t *testing.T) {
-	s, err := gqlparser.LoadSchema(`
-		type User {
-			name: String!
-		}
-	`)
-	require.Nil(t, err)
-
-	req, err := gqlparser.ParseQuery(`fragment subFieldNotDefined on User { Name }`)
-	require.Nil(t, err)
-
-	errs := Validate(s, &req)
-
-	fmt.Println(errs)
-}
 
 type Spec struct {
 	Name   string
@@ -39,6 +24,9 @@ func TestSpec(t *testing.T) {
 	var rawSchemas []string
 	readYaml("../spec/validation/schemas.yml", &rawSchemas)
 
+	var deviations map[string]*Spec
+	readYaml("../spec/validation/deviations.yml", &deviations)
+
 	var schemas []*gqlparser.Schema
 	for _, schema := range rawSchemas {
 		schema, err := gqlparser.LoadSchema(schema)
@@ -48,11 +36,11 @@ func TestSpec(t *testing.T) {
 		schemas = append(schemas, schema)
 	}
 
-	t.Run("FieldsOnCorrectType", runSpec(schemas, "../spec/validation/FieldsOnCorrectType.yml"))
-	t.Run("FragmentsOnCompositeTypes", runSpec(schemas, "../spec/validation/FragmentsOnCompositeTypes.yml"))
+	t.Run("FieldsOnCorrectType", runSpec(schemas, deviations, "../spec/validation/FieldsOnCorrectType.yml"))
+	t.Run("FragmentsOnCompositeTypes", runSpec(schemas, deviations, "../spec/validation/FragmentsOnCompositeTypes.yml"))
 }
 
-func runSpec(schemas []*gqlparser.Schema, filename string) func(t *testing.T) {
+func runSpec(schemas []*gqlparser.Schema, deviations map[string]*Spec, filename string) func(t *testing.T) {
 	var specs []Spec
 	readYaml(filename, &specs)
 	return func(t *testing.T) {
@@ -61,6 +49,12 @@ func runSpec(schemas []*gqlparser.Schema, filename string) func(t *testing.T) {
 				spec.Errors = nil
 			}
 			t.Run(spec.Name, func(t *testing.T) {
+				if deviation := deviations[spec.Name]; deviation != nil {
+					if deviation.Errors != nil {
+						spec.Errors = deviation.Errors
+					}
+				}
+
 				query, err := gqlparser.ParseQuery(spec.Query)
 				require.Nil(t, err)
 				errs := Validate(schemas[spec.Schema], &query)
@@ -70,7 +64,11 @@ func runSpec(schemas []*gqlparser.Schema, filename string) func(t *testing.T) {
 					spec.Errors[i].Rule = spec.Rule
 					spec.Errors[i].Locations = nil
 				}
-				require.Equal(t, spec.Errors, errs)
+				assert.Equal(t, spec.Errors, errs)
+
+				if t.Failed() {
+					t.Log("\nquery:", spec.Query)
+				}
 			})
 		}
 	}
