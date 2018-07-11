5103e3e4eb9cbf094fb1df1dfa4f469a239bb732
vvakame <vvakame+dev@gmail.com>
2018-07-11 14:55:41 +0900

    Implement KnownFragmentNames validator

--- validator/validate_test.go
+++ validator/validate_test.go
@@ -40,6 +40,7 @@ func TestSpec(t *testing.T) {
 	t.Run("FragmentsOnCompositeTypes", runSpec(schemas, deviations, "../spec/validation/FragmentsOnCompositeTypes.yml"))
 	t.Run("KnownArgumentNames", runSpec(schemas, deviations, "../spec/validation/KnownArgumentNames.yml"))
 	t.Run("KnownDirectives", runSpec(schemas, deviations, "../spec/validation/KnownDirectives.yml"))
+	t.Run("KnownFragmentNames", runSpec(schemas, deviations, "../spec/validation/KnownFragmentNames.yml"))
 
 	t.Run("ScalarLeafs", runSpec(schemas, deviations, "../spec/validation/ScalarLeafs.yml"))
 	t.Run("SingleFieldSubscriptions", runSpec(schemas, deviations, "../spec/validation/SingleFieldSubscriptions.yml"))
