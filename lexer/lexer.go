6f09140ccf7e195a23cdad3cc6a390915f631d26
Matti Andreas Nielsen <matti@denlillemand.com>
2019-01-05 19:57:41 +0100

    Remove redundat whitespace in switch in lexer.go

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -122,18 +122,14 @@ func (s *Lexer) ReadToken() (token Token, err *gqlerror.Error) {
 	case '#':
 		s.readComment()
 		return s.ReadToken()
-
 	case '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
 		return s.readName()
-
 	case '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 		return s.readNumber()
-
 	case '"':
 		if len(s.Input) > s.start+2 && s.Input[s.start:s.start+3] == `"""` {
 			return s.readBlockString()
 		}
-
 		return s.readString()
 	}
 
