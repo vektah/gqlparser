3e1c4f7f162f81e780122a5ec683bc87e315c4cd
vvakame <vvakame+dev@gmail.com>
2019-10-09 16:25:30 +0900

    make reproducable importer

--- validator/imported/export.sh
+++ validator/imported/export.sh
@@ -6,18 +6,26 @@ EXPORTER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $EXPORTER_ROOT
 
+GIT_REF=origin/master
+
+if [[ -f "$EXPORTER_ROOT/graphql-js-commit.log" ]] ; then
+  GIT_REF=$(cat "$EXPORTER_ROOT/graphql-js-commit.log")
+fi
+echo $GIT_REF
+
 if [[ -d "$REPO_DIR" ]] ; then
-    echo "fetching latest graphql-js"
+    echo "fetching graphql-js with ${GIT_REF}"
     cd $REPO_DIR
     git fetch origin master
-    git checkout origin/master
+    git checkout "$GIT_REF"
     git reset --hard
 else
-    echo "cloning graphql-js"
+    echo "cloning graphql-js with ${GIT_REF}"
     git clone --no-tags --single-branch -- https://github.com/graphql/graphql-js $REPO_DIR
     cd $REPO_DIR
-    git checkout origin/master
+    git checkout "$GIT_REF"
 fi
+git rev-parse HEAD > $EXPORTER_ROOT/graphql-js-commit.log
 
 cd $EXPORTER_ROOT
 
