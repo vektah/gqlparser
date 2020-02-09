1cdd9c855be2464cc458b34208bc91230df9982f
vvakame <vvakame+dev@gmail.com>
2020-02-09 18:00:35 +0900

    implement json.Unmarshaler

--- ast/path.go
+++ ast/path.go
@@ -2,10 +2,36 @@ package ast
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 )
 
+var _ json.Unmarshaler = (*Path)(nil)
+
 type Path []PathElement
+
+func (path *Path) UnmarshalJSON(b []byte) error {
+	var vs []interface{}
+	err := json.Unmarshal(b, &vs)
+	if err != nil {
+		return err
+	}
+
+	*path = make([]PathElement, 0, len(vs))
+	for _, v := range vs {
+		switch v := v.(type) {
+		case string:
+			*path = append(*path, PathName(v))
+		case int:
+			*path = append(*path, PathIndex(v))
+		case float64:
+			*path = append(*path, PathIndex(int(v)))
+		default:
+			return fmt.Errorf("unknown path element type: %T", v)
+		}
+	}
+	return nil
+}
 
 type PathElement interface {
 	isPathElement()
