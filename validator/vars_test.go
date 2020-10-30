4d219b8545b8c71c37cd1d9beae9963b81a2fc58
JatinDevDG <jatindev@dgraph.io>
2020-10-30 18:08:50 +0530

    fixed test

--- validator/vars_test.go
+++ validator/vars_test.go
@@ -154,10 +154,10 @@ func TestValidateVars(t *testing.T) {
 		t.Run("coerce to array", func(t *testing.T) {
 			q := gqlparser.MustLoadQuery(schema, `query foo($var: [InputType!]) { arrayArg(i: $var) }`)
 			vars, gerr := validator.VariableValues(schema, q.Operations.ForName(""), map[string]interface{}{
-				"var": "hello",
+				"name": "hello",
 			})
 			require.Nil(t, gerr)
-			require.EqualValues(t, map[string]interface{}(map[string]interface{}{"var": "hello"}), vars)
+			require.EqualValues(t, map[string]interface{}(map[string]interface{}{"name": "hello"}), vars)
 		})
 
 		t.Run("defaults", func(t *testing.T) {
