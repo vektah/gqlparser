e91275dd5c07614b6f58160da1e78a9005bab31b
vvakame <vvakame+dev@gmail.com>
2021-04-13 18:05:01 +0900

    apply shellcheck

--- validator/imported/export.sh
+++ validator/imported/export.sh
@@ -4,7 +4,7 @@ REPO_DIR=./graphql-js
 PATH=./node_modules/.bin:$PATH
 EXPORTER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $EXPORTER_ROOT
+cd "$EXPORTER_ROOT" || exit
 
 GIT_REF=origin/master
 
@@ -15,22 +15,22 @@ echo $GIT_REF
 
 if [[ -d "$REPO_DIR" ]] ; then
     echo "fetching graphql-js with ${GIT_REF}"
-    cd $REPO_DIR
+    cd "$REPO_DIR" || exit
     git fetch origin master
     git checkout "$GIT_REF"
     git reset --hard
 else
     echo "cloning graphql-js with ${GIT_REF}"
     git clone --no-tags --single-branch -- https://github.com/graphql/graphql-js $REPO_DIR
-    cd $REPO_DIR
+    cd "$REPO_DIR" || exit
     git checkout "$GIT_REF"
 fi
 git rev-parse HEAD > $EXPORTER_ROOT/graphql-js-commit.log
 
-cd $EXPORTER_ROOT
+cd "$EXPORTER_ROOT" || exit
 
 echo "installing js dependencies"
-npm install
+npm ci
 
 echo "exporting tests"
 babel-node ./export.js
