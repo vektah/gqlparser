176f77a5931d9cd5aa5a147e7699247b4c2c1c80
Shinnosuke Sawada <6warashi9@gmail.com>
2023-06-24 17:20:43 +0900

    refactor(ast): remove unused methods

--- ast/comment.go
+++ ast/comment.go
@@ -18,32 +18,6 @@ type CommentGroup struct {
 	List []*Comment
 }
 
-func (c *CommentGroup) Start() int {
-	if len(c.List) == 0 {
-		return 0
-	}
-	return c.List[0].Position.Start
-}
-
-func (c *CommentGroup) End() int {
-	if len(c.List) == 0 {
-		return 0
-	}
-	return c.List[len(c.List)-1].Position.End
-}
-
-func (c *CommentGroup) Text() string {
-	if c == nil || len(c.List) == 0 {
-		return ""
-	}
-	var builder strings.Builder
-	for _, comment := range c.List {
-		builder.WriteString(comment.Text())
-		builder.WriteString("\n")
-	}
-	return builder.String()
-}
-
 func (c *CommentGroup) Dump() string {
 	if len(c.List) == 0 {
 		return ""
