c75a8780ee44e2331ebf8af2b96a3bd7168666b0
Steve Coffman <steve@khanacademy.org>
2025-06-28 01:16:18 -0400

    Provide alternative sync.Map implementation
    Signed-off-by: Steve Coffman <steve@khanacademy.org>

--- validator/rules/rules.go
+++ validator/rules/rules.go
@@ -1,16 +1,20 @@
 package rules
 
-import "github.com/vektah/gqlparser/v2/validator/core"
+import (
+	"sync"
+
+	"github.com/vektah/gqlparser/v2/validator/core"
+)
 
 // Rules manages GraphQL validation rules.
 type Rules struct {
-	rules map[string]core.RuleFunc
+	rules *sync.Map
 }
 
 // NewRules creates a Rules instance with the specified rules.
 func NewRules(rs ...core.Rule) *Rules {
 	r := &Rules{
-		rules: make(map[string]core.RuleFunc),
+		rules: &sync.Map{},
 	}
 
 	for _, rule := range rs {
@@ -60,13 +64,16 @@ func NewDefaultRules() *Rules {
 // AddRule adds a rule with the specified name and rule function to the rule set.
 // If a rule with the same name already exists, it will not be added.
 func (r *Rules) AddRule(name string, ruleFunc core.RuleFunc) {
+	if r == nil {
+		// nonsensical, hopefully impossible
+		return
+	}
 	if r.rules == nil {
-		r.rules = make(map[string]core.RuleFunc)
+		// this is probably a mistake if we get here
+		r.rules = &sync.Map{}
 	}
-
-	if _, exists := r.rules[name]; !exists {
-		r.rules[name] = ruleFunc
-	}
+	// load only if a key doesn't already exist.
+	_, _ = r.rules.LoadOrStore(name, ruleFunc)
 }
 
 // GetInner returns the internal rule map.
@@ -75,24 +82,41 @@ func (r *Rules) GetInner() map[string]core.RuleFunc {
 	if r.rules == nil {
 		return make(map[string]core.RuleFunc)
 	}
-	return r.rules
+
+	innerCopy := make(map[string]core.RuleFunc)
+	r.rules.Range(func(key, value any) bool {
+		sKey, sok := key.(string)
+		vKey, vok := value.(core.RuleFunc)
+		if sok && vok {
+			innerCopy[sKey] = vKey
+		}
+		return true
+	})
+
+	return innerCopy
 }
 
 // RemoveRule removes a rule with the specified name from the rule set.
 // If no rule with the specified name exists, it does nothing.
 func (r *Rules) RemoveRule(name string) {
 	if r.rules != nil {
-		delete(r.rules, name)
+		r.rules.Delete(name)
 	}
 }
 
 // ReplaceRule replaces a rule with the specified name with a new rule function.
 // If no rule with the specified name exists, it does nothing.
 func (r *Rules) ReplaceRule(name string, ruleFunc core.RuleFunc) {
-	if r.rules == nil {
-		r.rules = make(map[string]core.RuleFunc)
+	if r == nil {
+		// nonsensical, hopefully impossible
+		return
 	}
-	if _, exists := r.rules[name]; exists {
-		r.rules[name] = ruleFunc
+	if r.rules == nil {
+		// this is probably a mistake if we get here
+		r.rules = &sync.Map{}
+	}
+	oldRule, ok := r.rules.Load(name)
+	if ok {
+		r.rules.CompareAndSwap(name, oldRule, ruleFunc)
 	}
 }
