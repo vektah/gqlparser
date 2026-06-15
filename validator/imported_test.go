f60276029ad832b9a370da45e1f2997a540b7e13
Roman A. Grigorovich <ragrigorov@mts.ru>
2026-06-15 14:34:06 +0300

    build(deps): migrate yaml import to go.yaml.in/yaml/v3
    Switch from gopkg.in/yaml.v3 to the canonical go.yaml.in/yaml/v3 module path.

--- validator/imported_test.go
+++ validator/imported_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v3"
 
 	"github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
